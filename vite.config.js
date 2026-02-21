60576a98b0902bd88463303f7bddfceac6e494cd
Michael Bower <michaelbower92@gmail.com>
2026-02-21 09:25:17 -0800

    Remove duplicate vite config; fix GitHub pages base

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({
-    plugins: [react()],
-    base: "/SpriteSmith/", // must match your GitHub repo name EXACTLY
-});
